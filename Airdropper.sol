55ab5e926803765ca0740fc238d09db0926c9d08
Narcis <narcisc2007@gmail.com>
2018-04-24 15:27:22 +0300

    refactoring

--- Airdropper.sol
+++ Airdropper.sol
@@ -5,9 +5,7 @@ import "./ERC20.sol";
 
 contract Airdropper is Ownable {
 
-    function multisend(address _tokenAddr, address[] dests, uint256[] values)
-    onlyOwner
-    returns (uint256) {
+    function multisend(address _tokenAddr, address[] dests, uint256[] values) onlyOwner returns (uint256) {
         uint256 i = 0;
         while (i < dests.length) {
             ERC20(_tokenAddr).transfer(dests[i], values[i]);
